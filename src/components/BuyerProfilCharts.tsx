c5a72ade7d6e6d375dec3b8464bb9cb35e07f0dd
david keukeu <davidkeukeu8@gmail.com>
2023-08-19 23:42:12 +0200

    transactions graphics

---
+++ src/components/BuyerProfilCharts.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react'
+import {} from 'recharts'
+
+const BuyerProfileCharts:FunctionComponent = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default BuyerProfileCharts;
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent } from "react";
+import TransactionChart from "./transactionChart";
+import BuyerProfileCharts from "./BuyerProfilCharts";
 
 import DashboardStatsGrid from "./DashboardStatsGrid";
 
 const Dashboard: FunctionComponent = () => {
   return (
-    <div className="flex gap-4">
+    <div className="flex flex-col gap-4">
         <DashboardStatsGrid />
+        <div className="flex flex-row gap-4 w-full">
+            <TransactionChart />
+            <BuyerProfileCharts />
+        </div>
     </div>
   );
 };
